0d332aeeb48007d60486ba58144b813589c33003
yurio <vika.tsyganova.dubna@gmail.com>
2022-04-18 23:26:14 +0300

    надо добавить фото и удаление элементов

--- forms/add_job.py
+++ forms/add_job.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 class AddJobForm(FlaskForm):
     product = StringField('Product Title', validators=[DataRequired()])
     price = IntegerField("Price")
-    team_leader = IntegerField("Team Leader id")
+    name = StringField("Your company name")
     description = StringField("Description")
     bargaining = BooleanField("Bargaining possible?")
     submit = SubmitField('Submit')
